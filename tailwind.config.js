e7f16996661f89db54394377ffb8e679b92ede91
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-10-10 16:08:02 +0530

    all updated

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust the file types as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
